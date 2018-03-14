2e8425412313fe6d317bcb43d80bb3283fc2b28f
salonikalsekar <salonikalsekar1@gmail.com>
2018-03-14 11:57:03 -0400

    final
    Final Project

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+
+const keys = require("./config/keys");
+require("./models/User");
+require("./models/Surveys");
+
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+const authRoutes = require("./routes/authRoutes");
+const billingRoutes= require('./routes/billingRoutes');
+const surveyRoutes= require('./routes/surveyRoutes');
+
+
+authRoutes(app);
+billingRoutes(app);
+surveyRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  
+  app.use(express.static('client/build'));
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("We've now got a server!");
+  console.log(`Your routes will be running on http://localhost:${PORT}`);
+});
